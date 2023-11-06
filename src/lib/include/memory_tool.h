9c92b0faa3b9b42ea63428c0924ddda61a1c47c1
EmilioRivera0 <elcholula619@gmail.com>
2023-11-05 22:27:10 -0600

    Remote process memory readed and copied successfully and more advances in the program

--- src/lib/include/memory_tool.h
+++ src/lib/include/memory_tool.h
@@ -3,13 +3,38 @@
 
 // necessary libraries -------->
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include <string.h>
+#include <ctype.h>
+#include <sys/uio.h>
+#include <sys/types.h>
 
 #include "structs.h"
 #include "macros.h"
+
+// macro definition -------->
+#define MAX_USER_INPUT_LEN 100
 
 // functions declaration -------->
 short int memory_menu(struct Program_Data*);
 struct iovec* initialize_remote_iovec(struct Program_Data*, const short int);
 void memory_IO(struct Program_Data*);
+bool is_whole_number(char*);
+bool is_decimal_number(char*);
+short int str_len(char*);
+
+/* 
+ * function declaration copied from <bits/uio-ext.h>
+ * I needed to include it because the language server was constantly generating undeclared function errors
+ * this was done just for developing purposes, the compilation and execution were not afected by this small 
+ * error of the language server 
+*/
+extern ssize_t process_vm_readv (pid_t, const struct iovec *,
+				 unsigned long int,
+				 const struct iovec *,
+				 unsigned long int,
+				 unsigned long int)
+  __THROW;
 
 #endif // !MEMORY_TOOL_H
--- src/lib/src/memory_tool.c
+++ src/lib/src/memory_tool.c
@@ -6,8 +6,9 @@ short int memory_menu(struct Program_Data* pd){
   short int op = 0, count = 1;
   struct Memory_Sector* temp = pd->memory_sectors;
   // show all the writable memory sectors and ask the user to choose one to work with
+  printf("\n");
   while (temp != NULL) {
-    printf("%d. %s \n\tSize:%ld Bytes \n", count, temp->name, temp->size);
+    printf("%d. %s \n\t%p - %p\n\tSize: %ld Bytes \n", count, temp->name, temp->start, temp->end, temp->size);
     count++;
     temp = temp->next;
   }
@@ -17,9 +18,37 @@ short int memory_menu(struct Program_Data* pd){
   return op;
 }
 
+
+bool is_whole_number(char* str){
+  while (*str != '\0') {
+    if (!isdigit(*str))
+      return false;
+    str++;
+  }
+  return true;
+}
+
+bool is_decimal_number(char* str){
+  while (*str != '\0') {
+    if (!isdigit(*str) && *str!='.')
+      return false;
+    str++;
+  }
+  return true;
+}
+
+short int str_len(char* str){
+  short int len = 0;
+  while (*str != '\0') {
+    len++;
+    str++;
+  }
+  return len;
+}
+
 struct iovec* initialize_remote_iovec(struct Program_Data* pd, const short int op){
   short int it = 1;
-  struct iovec* remote_iovec = NULL;
+  struct iovec* remote_iovec = (struct iovec*)malloc(sizeof(struct iovec));
   struct Memory_Sector* mem_sec = pd->memory_sectors;
   
   // move mem_sec to the Memory_Sector struct that the user specified
@@ -27,13 +56,20 @@ struct iovec* initialize_remote_iovec(struct Program_Data* pd, const short int o
     mem_sec = mem_sec->next;
     it++;
   }
+  // initialize remote iovec with chosen memory sector
+  remote_iovec->iov_base = mem_sec->start;
+  remote_iovec->iov_len = mem_sec->size;
 
   return remote_iovec;
 }
 
 void memory_IO(struct Program_Data* pd){
   short int op = 0;
+  ssize_t transfered_bytes = 0;
+  char usr_input[MAX_USER_INPUT_LEN];
+  // declare struct for remote iovec and declare and allocate memory space for local iovec struct
   struct iovec* remote_mem_sec = NULL;
+  struct iovec* local_mem_sec = malloc(sizeof(struct iovec));
   
   while (true) {
     op = memory_menu(pd);
@@ -42,5 +78,27 @@ void memory_IO(struct Program_Data* pd){
       break;
     // initialize remote iovec from the chosen memory sector of target process
     remote_mem_sec = initialize_remote_iovec(pd, op);
+    // initialize local iovec struct based on the remote iovec data
+    // allocate space for the buffer that will store the memory readed from the remote process
+    local_mem_sec->iov_base = malloc(remote_mem_sec->iov_len);
+    local_mem_sec->iov_len = remote_mem_sec->iov_len;
+    // read the remote process memory sector and store it in the allocated buffer
+    transfered_bytes = process_vm_readv(pd->pid, local_mem_sec, 1, remote_mem_sec, 1, 0);
+    // check if bytes were transfered
+    if (transfered_bytes <= 0) {
+      printf("Program failed to read process %d memory: %s\n\n", pd->pid, strerror(errno));
+      PROGRAM_ERROR
+    }
+    printf("\nRemote Process Memory Copied Successfully Into Program's Buffer\n\n");
+    // clean usr_input array content
+    memset(usr_input, '\0', MAX_USER_INPUT_LEN);
+    // ask for user to input the value to search for in the buffer
+    printf("Indicate the value to search for (Rational Numbers/Character/String):\n\n\nValue > ");
+    scanf(" %s", usr_input);
+    printf("%s", usr_input);
+
+    // free allocated memory from heap
+    free(remote_mem_sec);
+    free(local_mem_sec->iov_base);
   }
 }
